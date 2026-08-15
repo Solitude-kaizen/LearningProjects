0b4cc558767795e148c73c4f665954df7d7baa83
Solitude-Kaizen <dan.yhel.18.2006@gmail.com>
2026-08-16 03:09:07 +0800

    Add continuous menu and goal management

--- src/solitude_kaizen/main.py
+++ src/solitude_kaizen/main.py
@@ -13,9 +13,39 @@ current_goal = profile.get("current_goal")
 
 print(name, "V" + version)
 print("Hello,", user_name + ".")
-print("I remembered your name from my local memory.")
 
 if current_goal:
     print("Your current goal is:", current_goal)
-else:
-    print("I don't have a current goal saved yet.")
+
+while True:
+    print()
+    print("What would you like to do?")
+    print("1. View current goal")
+    print("2. Change current goal")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        if current_goal:
+            print("Your current goal is:", current_goal)
+        else:
+            print("You do not have a current goal saved.")
+
+    elif choice == "2":
+        new_goal = input("Enter your new goal: ")
+
+        profile["current_goal"] = new_goal
+        current_goal = new_goal
+
+        with open(profile_path, "w") as file:
+            json.dump(profile, file, indent=4)
+
+        print("Your goal has been updated.")
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid option. Please choose again.")
